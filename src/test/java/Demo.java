21204162da001642fc9a794d3a745da4edda2668
Vyper99 <111468891+Vyper99@users.noreply.github.com>
2022-08-31 20:34:15 -0400

    Merge branch 'main' of https://github.com/TechInsider-LLC/BDD-TestAutomation-Tech-Titans

--- src/test/java/Demo.java
+++ src/test/java/Demo.java
@@ -1,5 +1,7 @@
 public class Demo {
     public static void main(String[] args) {
-       System.out.println("Akobir");
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://demo.ebanq.com/log-in");
 }
 
