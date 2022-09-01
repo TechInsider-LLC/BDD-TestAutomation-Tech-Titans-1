b3e183c7b0a033f7bba570fe3e363ea7c87865e6
Vyper99 <111468891+Vyper99@users.noreply.github.com>
2022-08-31 20:45:09 -0400

    Update Demo.java

--- src/test/java/Demo.java
+++ src/test/java/Demo.java
@@ -1,7 +1,12 @@
-public class Demo {
-    public static void main(String[] args) {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.get("https://demo.ebanq.com/log-in");
-}
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+public class Demo {
+
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://demo.ebanq.com/log-in");
+    }
+}
