b9309a1d14648c8cff407b6d02a5145f3fb3eac1
AzamatSiro <111468715+AzamatSiro@users.noreply.github.com>
2022-08-31 22:52:59 -0400

    commit

--- src/test/java/Demo.java
+++ src/test/java/Demo.java
@@ -1,10 +1,5 @@
 public class Demo {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Azamat");
-    }
-=======
-       System.out.println("Akobir");
->>>>>>> 18696030f3c4c80db4310939ebfc59f607dbe270
+       System.out.println("Azamat");
 }
 
