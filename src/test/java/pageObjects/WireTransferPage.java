b4645c4505eee1ed37a4c84ea1e72eb0ea8072ff
Akobirhon <khikmatulloev@gmail.com>
2022-09-05 14:22:00 -0400

    E2E | Outgoing Wire Transfer

---
+++ src/test/java/pageObjects/WireTransferPage.java
@@ -0,0 +1,103 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import stepDefinitions.Hooks;
+
+
+public class WireTransferPage {
+
+    WebDriver getDriver = Hooks.getDriver();
+
+    public String validUsername = "Demo-User";
+    public String validPassword = "Demo-Access1";
+    public String validAddress = "123 Bronx Ave";
+    public String swiftCode = "SMCOGB2L";
+    public String customerName = "Adam";
+    public String customerCity = "Bronx";
+    public String customerState ="New York";
+    public String accountIban = "AL35202111090000000001234567";
+    public String refMessage = "Test";
+    public String amountToTransfer = "1000";
+
+    public String URL = "https://demo.ebanq.com/log-in";
+    public void load(){
+        getDriver.get(URL);
+    }
+    @FindBy(xpath = "//input[@placeholder='Enter username or email address']")
+    public WebElement usernameInput;
+
+    @FindBy(xpath = "//input[@placeholder='Enter password']")
+    public WebElement passwordInput;
+
+    @FindBy(css = ".header .aside-bar__menu-toggle")
+    public WebElement headerMenu;
+
+    @FindBy(css = "div[ng-reflect-router-link='/transfer']")
+    public WebElement transfers;
+
+    @FindBy(css = "div[ng-reflect-router-link='outgoing-wire-transfer']")
+    public WebElement outgoingWireTransfer;
+
+    @FindBy(css = ".select-container-large .ng-select-container")
+    public WebElement selectAccount;
+
+    @FindBy(css = ".ng-option-marked")
+    public WebElement selectChecking;
+
+    @FindBy(css = "#beneficiary-bankSwiftBic")
+    public WebElement beneficiarySwiftBankCode;
+
+    @FindBy(css = "#customer-name")
+    public WebElement customerName1;
+
+    @FindBy(css = "#customer-address")
+    public WebElement customerAddress;
+
+    @FindBy(css = "#customer-city")
+    public WebElement customerCity1;
+
+    @FindBy(css = "#customer-state-prov-region")
+    public WebElement customerState1;
+
+    @FindBy(css = "[ng-reflect-name='customerCountry']")
+    public WebElement customerCountrySelect;
+
+    @FindBy(css = ".ng-option:nth-child(235) .ng-option-label")
+    public WebElement selectsUSA;
+
+    @FindBy(css = "#acc-iban")
+    public WebElement accountIban1;
+
+    @FindBy(css = "#ref-message")
+    public WebElement refMessage1;
+
+    @FindBy(css = "#outgoingAmountOptionsPipe")
+    public WebElement amountToTransfer1;
+
+    @FindBy(css = "[ng-reflect-name='referenceCurrencyCode']")
+    public WebElement selectCurrency;
+
+    @FindBy(css = ".ng-option:nth-child(28) .ng-option-label")
+    public WebElement selectUSA$;
+
+    @FindBy(css = "body")
+    public WebElement body;
+
+    @FindBy(css = ".bottom .def-btn-success ")
+    public WebElement continueButton;
+
+    @FindBy(css = ".header .controls__logout")
+    public WebElement logOutLink;
+
+    @FindBy(css = ".error-word")
+    public WebElement errorField;
+
+    public String logOut = "Log Out";
+
+    public String fieldsRequired = "Field is required.";
+
+
+
+}
---
+++ src/test/java/stepDefinitions/WireTransfer.java
@@ -0,0 +1,109 @@
+package stepDefinitions;
+import io.cucumber.java.en.*;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.support.PageFactory;
+import pageObjects.WireTransferPage;
+
+import static org.junit.Assert.assertEquals;
+import static stepDefinitions.Hooks.getDriver;
+
+public class WireTransfer {
+
+    String username;
+    String password;
+    String address;
+    String swiftCode;
+    String customerName;
+    String customerCity;
+    String customerState;
+    String accountIban;
+    String refMessage;
+    String amountToTransfer;
+
+
+    WireTransferPage transferPage = PageFactory.initElements(getDriver(), WireTransferPage.class);
+
+    @Given("User on the Ebanq page")
+    public void user_on_the_ebanq_page() {
+        username = transferPage.validUsername;
+        password = transferPage.validPassword;
+        address = transferPage.validAddress;
+        swiftCode = transferPage.swiftCode;
+        customerName = transferPage.customerName;
+        customerCity = transferPage.customerCity;
+        customerState = transferPage.customerState;
+        accountIban = transferPage.accountIban;
+        refMessage = transferPage.refMessage;
+        amountToTransfer = transferPage.amountToTransfer;
+        transferPage.load();
+    }
+    @When("User Logs in and click on transfers then click on Outgoing wire Transfer")
+    public void user_logs_in_and_click_on_transfers_then_click_on_outgoing_wire_transfer() throws InterruptedException {
+        Thread.sleep(3000);
+        transferPage.usernameInput.sendKeys(transferPage.validUsername);
+        transferPage.passwordInput.sendKeys(transferPage.validPassword,Keys.ENTER);
+        Thread.sleep(4000);
+        transferPage.headerMenu.click();
+        Thread.sleep(2000);
+        transferPage.transfers.click();
+        Thread.sleep(3000);
+        transferPage.outgoingWireTransfer.click();
+
+
+    }
+    @When("Select account and fills the form then click on continue")
+    public void select_account_and_fills_the_form_then_click_on_continue() throws InterruptedException {
+        Thread.sleep(2000);
+        transferPage.selectAccount.click();
+        Thread.sleep(1000);
+        transferPage.selectChecking.click();
+        transferPage.beneficiarySwiftBankCode.sendKeys(swiftCode);
+        transferPage.customerName1.sendKeys(customerName);
+        transferPage.customerAddress.sendKeys(address);
+        transferPage.customerCity1.sendKeys(customerCity);
+        transferPage.customerState1.sendKeys(customerState);
+        transferPage.customerCountrySelect.click();
+        transferPage.selectsUSA.click();
+        transferPage.accountIban1.sendKeys(accountIban);
+        transferPage.refMessage1.sendKeys(refMessage);
+        transferPage.amountToTransfer1.sendKeys(amountToTransfer);
+        transferPage.selectCurrency.click();
+        transferPage.selectUSA$.click();
+        Thread.sleep(3000);
+        transferPage.body.sendKeys(Keys.CONTROL,Keys.END);
+        transferPage.continueButton.click();
+    }
+
+    @When("User on Transfer details page Scrolls down end click on continue")
+    public void user_on_transfer_details_page_scrolls_down_end_click_on_continue() throws InterruptedException {
+        Thread.sleep(3000);
+        transferPage.body.sendKeys(Keys.CONTROL,Keys.END);
+        transferPage.continueButton.click();
+    }
+    @Then("Users Transaction success")
+    public void users_transaction_success() throws InterruptedException {
+        Thread.sleep(2000);
+        String expected = transferPage.logOut;
+        String actual = transferPage.logOutLink.getText().trim();
+        assertEquals(expected,actual);
+    }
+
+
+
+
+    @When("User Scroll down click on continue button")
+    public void user_scroll_down_click_on_continue_button() throws InterruptedException {
+        Thread.sleep(3000);
+        transferPage.body.sendKeys(Keys.CONTROL,Keys.END);
+        transferPage.continueButton.click();
+    }
+    @Then("User Should get error message")
+    public void user_should_get_error_message() throws InterruptedException {
+        Thread.sleep(2000);
+        String expected = transferPage.fieldsRequired;
+        String actual = transferPage.errorField.getText().trim();
+        assertEquals(expected,actual);
+    }
+}
+
+
