029e78f659ead527b7c6cba7661ec6928b0fabb8
Surensever <surenatashkaria@gmail.com>
2022-09-21 20:43:57 -0400

    Updated

--- src/test/java/stepDefinitions/TransferBetweenUser.java
+++ src/test/java/stepDefinitions/TransferBetweenUser.java
@@ -17,39 +17,21 @@ import java.util.concurrent.TimeUnit;
 import static io.restassured.RestAssured.given;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static utility.Hooks.getDriver;
 
 public class TransferBetweenUser {
-    WebDriver driver;
+
     String id;
     @Given("User successfully log in")
     public void user_successfully_log_in() throws InterruptedException {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://demo.ebanq.com/log-in");
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.findElement(By.cssSelector(".fields-container [type=\"email\"]")).sendKeys("Demo-User");
-        driver.findElement(By.cssSelector(".fields-container [type=\"password\"]")).sendKeys("Demo-Access1", Keys.ENTER);
+        getDriver().get("https://demo.ebanq.com/log-in");
+        getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        getDriver().findElement(By.cssSelector(".fields-container [type=\"email\"]")).sendKeys("Demo-User");
+        getDriver().findElement(By.cssSelector(".fields-container [type=\"password\"]")).sendKeys("Demo-Access1", Keys.ENTER);
         //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        Thread.sleep(5000);
-        driver.findElement(By.cssSelector("div[ng-reflect-router-link='/transfer']")).click();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.findElement(By.cssSelector("div[ng-reflect-router-link=\"transfer-between-users\"]")).click();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.findElement(By.cssSelector("div[class=\"ng-value-container\"]")).click();
         Thread.sleep(2000);
-        driver.findElement(By.cssSelector("DIV[class=\"ng-option ng-option-marked\"]")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.cssSelector("#username")).sendKeys("jsmith");
-        Thread.sleep(2000);
-        driver.findElement(By.cssSelector("#account")).sendKeys("EBQ22123487654");
-        Thread.sleep(2000);
-        driver.findElement(By.cssSelector("[ng-reflect-name=\"outgoingAmount\"]")).sendKeys("1");
-        Thread.sleep(2000);
-        driver.findElement(By.cssSelector("[ng-reflect-name=\"outgoingAmount\"]")).submit();
-        Thread.sleep(2000);
-        driver.findElement(By.cssSelector("[ng-reflect-name=\"outgoingAmount\"]")).submit();
-        Thread.sleep(2000);
-        driver.findElement(By.cssSelector(".popup-message [href=\"/transfer\"]")).getText().trim();
+
+
 
 
 
@@ -58,35 +40,41 @@ public class TransferBetweenUser {
     }
     @When("User click on TransferBetweenUser")
     public void user_click_on_transfer_between_user() throws InterruptedException {
-        driver.findElement(By.cssSelector("div[ng-reflect-router-link='/transfer']")).click();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.findElement(By.cssSelector("div[ng-reflect-router-link=\"transfer-between-users\"]")).click();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        Thread.sleep(2000);
+        getDriver().findElement(By.cssSelector("div[ng-reflect-router-link='/transfer']")).click();
+        getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        getDriver().findElement(By.cssSelector("div[ng-reflect-router-link=\"transfer-between-users\"]")).click();
+        getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        getDriver().findElement(By.cssSelector("div[class=\"ng-value-container\"]")).click();
+        getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        getDriver().findElement(By.cssSelector("DIV[class=\"ng-option ng-option-marked\"]")).click();
+        getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+
+
 
     }
     @When("User fill out the form and confirm")
     public void user_fill_out_the_form_and_confirm() throws InterruptedException {
-        driver.findElement(By.cssSelector("div[class=\"ng-value-container\"]")).click();
+        getDriver().findElement(By.cssSelector("#username")).sendKeys("jsmith");
+        getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        getDriver().findElement(By.cssSelector("#account")).sendKeys("EBQ22123487654");
+        getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        getDriver().findElement(By.cssSelector("[ng-reflect-name=\"outgoingAmount\"]")).sendKeys("1");
         Thread.sleep(2000);
-        driver.findElement(By.cssSelector("DIV[class=\"ng-option ng-option-marked\"]")).click();
+        getDriver().findElement(By.cssSelector("[ng-reflect-name=\"outgoingAmount\"]")).submit();
         Thread.sleep(2000);
-        driver.findElement(By.cssSelector("#username")).sendKeys("jsmith");
+        getDriver().findElement(By.cssSelector("[ng-reflect-name=\"outgoingAmount\"]")).submit();
         Thread.sleep(2000);
-        driver.findElement(By.cssSelector("#account")).sendKeys("EBQ22123487654");
-        Thread.sleep(2000);
-        driver.findElement(By.cssSelector("[ng-reflect-name=\"outgoingAmount\"]")).sendKeys("1");
-        Thread.sleep(2000);
-        driver.findElement(By.cssSelector("[ng-reflect-name=\"outgoingAmount\"]")).submit();
-        Thread.sleep(2000);
-        driver.findElement(By.cssSelector("[ng-reflect-name=\"outgoingAmount\"]")).submit();
-        Thread.sleep(2000);
+
+
 
     }
     @Then("User get verify message")
     public void user_get_verify_message() {
        //actual=  driver.findElement(By.cssSelector(".popup-message [href=\"/transfer\"]")).getText().trim();
         String expected = " Your request has been sent for approval. ".trim();
-        String actual = driver.findElement(By.cssSelector(".success-popup .popup-message")).getText();
+        String actual = getDriver().findElement(By.cssSelector(".success-popup .popup-message")).getText();
         System.out.println(actual);
         assertTrue(actual.contains(expected));
 
