cdc54ad0c1e5f388d5051d484bbdf62a91cacccf
racoonbest <91798907+racoonbest@users.noreply.github.com>
2022-10-05 20:35:52 -0400

    Update ReRunner.java

--- src/test/java/utility/ReRunner.java
+++ src/test/java/utility/ReRunner.java
@@ -6,8 +6,8 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = {"@target/rerun.txt"},
         glue = {"stepDefinitions","utility"},
-        plugin = {"rerun:target/rerun.txt",
-                  "pretty", "html:target/rerun-cucumber-reports.html"}
+        plugin = {"rerun:target/rerun/rerun.txt",
+                  "pretty", "html:target/rerun/rerun-cucumber-reports.html"}
         )
 
 public class ReRunner extends AbstractTestNGCucumberTests {
